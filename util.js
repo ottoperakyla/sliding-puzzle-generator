d7a7efb7854b8e7853ae543c7a197be66bdacb82
Otto Peräkylä <otto.perakyla@gmail.com>
2018-12-19 08:45:07 +0200

    remove var

--- util.js
+++ util.js
@@ -1,8 +1,8 @@
 export function relativeCoords ( event ) {
-  var bounds = event.target.getBoundingClientRect();
-  var x = event.clientX - bounds.left;
-  var y = event.clientY - bounds.top;
-  return {x: x, y: y};
+  const bounds = event.target.getBoundingClientRect()
+  const x = event.clientX - bounds.left
+  const y = event.clientY - bounds.top
+  return {x: x, y: y}
 }
 
 export function shuffle(a) {
@@ -10,5 +10,5 @@ export function shuffle(a) {
       const j = Math.floor(Math.random() * (i + 1));
       [a[i], a[j]] = [a[j], a[i]]
   }
-  return a;
+  return a
 }
